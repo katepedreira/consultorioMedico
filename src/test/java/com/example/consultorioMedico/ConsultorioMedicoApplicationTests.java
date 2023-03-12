67ee5a84d2dfd0e222ee477e21bc97ed9d8e5dd2
Katherine.Pedreira <kakamarpe@gmail.com>
2023-03-12 17:54:15 -0300

    first commit

---
+++ src/test/java/com/example/consultorioMedico/ConsultorioMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultorioMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
